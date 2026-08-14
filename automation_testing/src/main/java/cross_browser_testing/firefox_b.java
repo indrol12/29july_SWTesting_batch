1f6fe10ba00d9f66c2ffb3095849fdc3eaa9ccf5
indrol12 <indrolofficial@gmail.com>
2026-08-14 16:37:48 +0530

    firefoxdriver added

---
+++ automation_testing/src/main/java/cross_browser_testing/firefox_b.java
@@ -0,0 +1,10 @@
+package cross_browser_testing;
+
+import org.openqa.selenium.firefox.*;
+public class firefox_b {
+public static void main(String[] args) {
+	
+	FirefoxDriver fx=new FirefoxDriver();
+	fx.close();
+}
+}
