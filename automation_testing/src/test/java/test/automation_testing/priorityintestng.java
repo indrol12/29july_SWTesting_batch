c56857f7261f4f57bfd14eccb17bfb784c2ef760
indrol12 <indrolofficial@gmail.com>
2026-08-18 16:04:06 +0530

    priority in testng added

---
+++ automation_testing/src/test/java/test/automation_testing/priorityintestng.java
@@ -0,0 +1,25 @@
+package test.automation_testing;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class priorityintestng {
+
+	@Test
+	void show()
+	{
+	
+		System.out.println("show method runs...");
+	}
+	@Test
+	void display()
+	{
+		System.out.println("display  method runs...");
+	}
+	@Test
+	void add()
+	{
+		System.out.println("add method runs...");
+	
+	}
+}
